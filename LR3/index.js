95a6d9835fab69e628bff5adfdc672caf259b51c
Reshetar Dmytro <reshetar.dmytro@student.uzhnu.edu.ua>
2025-10-07 11:56:49 +0300

    LR3

---
+++ LR3/index.js
@@ -0,0 +1,13 @@
+let a=4
+if (a%2===0){
+    console.log("Число парне")
+}
+else {
+    console.log("Число не парне")
+}
+let b=10
+console.log(b%2===0? 'Число парне':'Число не парне')
+let name='Dmytro'
+let age=19
+if(name=="Dmytro" || age==18)
+    console.log("Мене звати "+ name +" і мені "+ age +" років ")
