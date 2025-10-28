e1bf7053aab439c5e6478578b5f8065db14df0e0
Reshetar Dmytro <reshetar.dmytro@student.uzhnu.edu.ua>
2025-10-28 12:04:37 +0200

    LR5

---
+++ LR5/index.js
@@ -0,0 +1,8 @@
+function fname(name,age){
+    console.log(`My name is ${name}`)
+    console.log(`My age is ${age}`)
+    const a=age*2
+    return a;
+}
+const b=fname('Dmytro',19);
+console.log(b);
