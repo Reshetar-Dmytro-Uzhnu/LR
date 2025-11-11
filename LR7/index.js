c60e333e81061bf3251a13c41f3824c319194951
Reshetar Dmytro <reshetar.dmytro@student.uzhnu.edu.ua>
2025-11-11 11:57:45 +0200

    LR7

---
+++ LR7/index.js
@@ -0,0 +1,17 @@
+// const sports=[
+//     { name: 'football', players: 22},
+//     { name: 'boxing', players: 2},
+//     {name: 'volleyball', players: 14},
+//     {name: 'hockey', players: 12}
+// ]
+// const  FSports= sports.filter((player)=> player.players > 13)
+// console.log(FSports)
+//
+// const SSports = sports.some(sport => sport.players/2 === 7)
+// console.log(SSports)
+//
+// const NSports = sports.find(sports => sports.players/2 === 7)
+// console.log(NSports.name)
+//
+// const RSports= sports.reduce((acc, sport)=> acc + sport.players,sports.length)
+// console.log(RSports)
